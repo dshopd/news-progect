8d62f54ce5c31f186547607a34be517996fe7910
dshopd <ushanskiu@yandex.ru>
2017-07-18 19:18:04 +0300

    'prav'

--- gulpfile.js
+++ gulpfile.js
@@ -68,11 +68,27 @@ gulp.task('img', function(){
 		.pipe(gulp.dest('dist/img'))
 });
 
+//script
+gulp.task("js", function () {
+	return gulp.src('app/js/main.js')
+		.pipe(gulp.dest('app/js/'))
+		.pipe(browserSync.stream());
+})
+
+gulp.task("script", function () {
+	return gulp.src([
+			'app/bower_components/jquery/dist/jquery.min.js',
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(gulp.dest('app/js/'))
+});
+
 
 //watch
 gulp.task('watch', function() {
 	gulp.watch('app/pug/**/*.pug', ['pug']);
 	gulp.watch('app/sass/*.sass', ['sass']);
+	gulp.watch('app/js/*.js', ['js']);
 });
 
 
@@ -88,6 +104,38 @@ gulp.task('browser-sync', function() {
 
 
 
+//clean
+gulp.task('clean', function(){
+	return del.sync('dist');
+});
+
+gulp.task('clean', function(){
+	return cache.clearAll();
+});
 
 
-gulp.task('default', ['browser-sync', 'watch', 'pug', 'sass',]);
+
+gulp.task('default', ['browser-sync', 'watch', 'pug', 'sass', 'js']);
+
+gulp.task('build', ['clean', 'img'], function() {
+
+	var buildCss = gulp.src([
+		'app/css/main.css',
+	])
+		.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src(['app/fonts/**/*'])
+		.pipe(gulp.dest('dist/fonts'));
+
+
+
+	var buildHtml = gulp.src(['app/*.html'])
+		.pipe(gulp.dest('dist/'));
+
+	var buildJs = gulp.src(['app/js/*.js'])
+		.pipe(gulp.dest('dist/js'));
+
+	var buildSvg = gulp.src(['app/img/**/*.svg'])
+		.pipe(gulp.dest('dist/img'));
+
+});
